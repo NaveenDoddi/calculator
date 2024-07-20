62f376b6b1efced757892077b6c7a3aec5b8a0fd
Naveen Doddi <109824438+NaveenDoddi@users.noreply.github.com>
2024-07-20 13:13:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,64 +1,118 @@
-function sum(){
-    var input = document.getElementById("inpt").value;
-    let Array=[];
-    var inputvalue=input.split("+");
-    
-    Array=inputvalue;
-    let num1=parseInt(Array[0]);
-    let num2=parseInt(Array[1]);
-    document.getElementById("output").append(num1+num2);
-    
-}
-function sub(){
-    var input = document.getElementById("inpt").value;
-    let Array=[];
-    var inputvalue=input.split("-");
-    
-    Array=inputvalue;
-    let num1=parseInt(Array[0]);
-    let num2=parseInt(Array[1]);
-    document.getElementById("output").append(num1-num2);
-    
-}
-function mul(){
-    var input = document.getElementById("inpt").value;
-    let Array=[];
-    var inputvalue=input.split("*");
-    
-    Array=inputvalue;
-    let num1=parseInt(Array[0]);
-    let num2=parseInt(Array[1]);
-    document.getElementById("output").append(num1*num2);
-}
-function div(){
-    var input = document.getElementById("inpt").value;
-    let Array=[];
-    var inputvalue=input.split("/");
-    
-    Array=inputvalue;
-    let num1=parseInt(Array[0]);
-    let num2=parseInt(Array[1]);
-    document.getElementById("output").append(num1/num2);
+
+function giveInput(element){
+    var value = element.innerText;
+    document.getElementById("input").append(value);
 }
 
-function per(){
-    var input = document.getElementById("inpt").value;
-    let Array=[];
-    var inputvalue=input.split("%");
-    
-    Array=inputvalue;
-    let num1=parseInt(Array[0]);
-    let num2=parseInt(Array[1]);
-    document.getElementById("output").append(num1%num2);
-}
 function ans(){
-    document.getElementById("inpt").value=" ";
+    document.getElementById("output").innerHTML=""
+    document.getElementById("outputLogo").style.display = "none"
+
+    var input = document.getElementById("input").innerHTML
+    
+    document.getElementById("output").append(eval(input))
+    // let Array=[];
+    // for(let x=0; x<input.length; x++){
+
+    //     if(input[x]=="+"){
+            
+    //         var inputvalue=input.split("+");
+    
+    //         Array=inputvalue;
+    //         let num1=parseFloat(Array[0]);
+    //         let num2=parseFloat(Array[1]);
+    //         document.getElementById("output").append(num1+num2);
+    //     }
+    //     if(input[x]=="-"){
+
+    //         var inputvalue=input.split("-");
+    
+    //         Array=inputvalue;
+    //         let num1=parseFloat(Array[0]);
+    //         let num2=parseFloat(Array[1]);
+    //         document.getElementById("output").append(num1-num2); 
+    //     }
+    //     if(input[x]=="*"){
+    //         var inputvalue=input.split("*");
+    
+    //         Array=inputvalue;
+    //         let num1=parseFloat(Array[0]);
+    //         let num2=parseFloat(Array[1]);
+    //         document.getElementById("output").append(num1*num2); 
+    //     }
+    //     if(input[x]=="/"){
+    //         var inputvalue=input.split("/");
+    
+    //         Array=inputvalue;
+    //         let num1=parseFloat(Array[0]);
+    //         let num2=parseFloat(Array[1]);
+    //         document.getElementById("output").append(num1/num2); 
+    //     }
+    //     if(input[x]=="%"){
+    //         var inputvalue=input.split("%");
+    
+    //         Array=inputvalue;
+    //         let num1=parseFloat(Array[0]);
+    //         let num2=parseFloat(Array[1]);
+    //         document.getElementById("output").append(num1%num2)
+    //     }
+
+    // }
+
 }
 function ac(){
-    document.getElementById("inpt").value=" ";
-    document.getElementById("output").innerHTML=" ";
-    document.para.write(" ");
-    
+    window.location.reload()
+}
+function del(){
+
+    var input = document.getElementById("input").innerHTML
+    var slicedData=input.slice(0, -1)
+    document.getElementById("input").innerHTML=" "
+    document.getElementById("input").append(slicedData)
+
+}
+
+
+function run(){
+    document.getElementById("inputLogo").style.display = "none"
+    document.getElementById("output").style.transform
+}
+
+function factorial(){
+    document.getElementById("input").append("!")
+    document.getElementById("outputLogo").style.display = "none"
+
+    var value = document.getElementById("input").innerText.split("!")[0]
+    var result = 1
+    for(let i = value; i > 1; i--){
+        result *= i
+    }
+    document.getElementById("output").append(result)
+
+}
+
+function factors(){
+    document.getElementById("outputLogo").style.display = "none"
+    var value = document.getElementById("input").innerText
+    var result = ''
+    for(let i = 1; i < value; i++){
+        if(value % i == 0){
+            result += i + ", "
+
+        }
+
+    }
+    result += value
+    document.getElementById("output").append(result)
+
+}
+
+function suareRoot(){
+    document.getElementById("outputLogo").style.display = "none"
+    var value = document.getElementById("input").innerText
+    var result = Math.sqrt(value)
+    document.getElementById("output").append(result)
+
 }
 
 // setTimeout(() => {window.location.reload();}, 1000);
